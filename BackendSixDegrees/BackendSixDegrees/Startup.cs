b9319f47f0dde55f5eb794280f206c54b8d8df93
EfraMonR <efrain.montealegre@gmail.com>
2024-03-14 09:27:47 -0500

    the code was documented at specific points in the backend project.

--- BackendSixDegrees/BackendSixDegrees/Startup.cs
+++ BackendSixDegrees/BackendSixDegrees/Startup.cs
@@ -32,10 +32,10 @@ namespace BackendSixDegrees
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Registra servicios de MediatR y Automapper
+            //Registra servicios de MediatR y Automapper desde la capa de negocio
             services.AddApplicationServices(Configuration);
 
-            //Registra servicios de base de datos
+            //Registra servicios de base de datos desde la capa de acceso de datos
             services.AddPersistenceServices(Configuration);
 
             services.AddControllers();
